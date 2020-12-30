f431bbdafdb83e6ab540f9b5fbf9a85c6615d818
ckomodo <chrissakwa@gmail.com>
2020-12-29 22:19:02 -0800

    UPDATE and DELETE route added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+const { read } = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -25,44 +26,86 @@ app.use(express.json());
 // =============================================================
 // require("./routes/api-routes.js")(app);
 
-app.get('/', function (req, res){
-    res.send("blog is coming soon")
-})
+app.get("/", function (req, res) {
+  res.send("blog is coming soon");
+});
 
 //USER routes
 
-//GET all users
-app.get('/api/users', function(req, res){
-    db.User.findAll().then(users=>{
-        res.json(users)
-    })
-})
+//GET/read all users
+app.get("/api/users", function (req, res) {
+  db.User.findAll().then((users) => {
+    res.json(users);
+  });
+});
 
 //POST/create users
-app.post('/api/users', function(req, res){
-    db.User.create({
-        username: req.body.username,
-        email:req.body.email,
-        password: req.body.password
-    }).then(newUser=>{
-        res.json(newUser)
-    }).catch(err=>{
-        res.status(500).json(err)
+app.post("/api/users", function (req, res) {
+  db.User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((newUser) => {
+      res.json(newUser);
     })
-})
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+//PUT/update a user
+app.put("/api/users/:id", function (req, res) {
+  db.User.update(
+    {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updateUser) => {
+      if (updateUser[0] === 0) {
+        res.status(404).json(updateUser);
+      } else {
+        console.log(updateUser);
+        res.json(updateUser);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-
-
-
-
-
-
+//DELETE a user
+app.delete("/api/users/:id", function (req, res) {
+  db.User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (data === 0) {
+        res.status(404).json(data);
+      } else {
+        res.json(data);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({force: false}).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
