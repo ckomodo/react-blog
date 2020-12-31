6ce4eba16be0d09735cf7856a2daa810b86c68bf
ckomodo <chrissakwa@gmail.com>
2020-12-30 18:12:39 -0800

    user and admin controllers added

---
+++ Controllers/AdminController.js
@@ -0,0 +1,80 @@
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+var db = require("./models");
+
+//USER routes
+
+//GET/read all Admins
+app.get("/api/admin", function (req, res) {
+    db.Admin.findAll().then((admins) => {
+      res.json(admins);
+    });
+  });
+  
+  //POST/create admin
+  app.post("/api/admin", function (req, res) {
+    db.Admin.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((newAdmin) => {
+        res.json(newAdmin);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  
+  //PUT/update admin
+  app.put("/api/admin/:id", function (req, res) {
+    db.Admin.update({
+      username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then((updateAdmin) => {
+        if (updateAdmin[0] === 0) {
+          res.status(404).json(updateAdmin);
+        } else {
+          console.log(updateAdmin);
+          res.json(updateAdmin);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  //DELETE a user
+  app.delete("/api/admin/:id", function (req, res) {
+    db.Admin.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((data) => {
+        if (data === 0) {
+          res.status(404).json(data);
+        } else {
+          res.json(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = Admin;
--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-const { read } = require("fs");
-
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -28,78 +26,6 @@ app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("blog is coming soon");
-});
-
-//USER routes
-
-//GET/read all users
-app.get("/api/users", function (req, res) {
-  db.User.findAll().then((users) => {
-    res.json(users);
-  });
-});
-
-//POST/create users
-app.post("/api/users", function (req, res) {
-  db.User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((newUser) => {
-      res.json(newUser);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
-
-//PUT/update a user
-app.put("/api/users/:id", function (req, res) {
-  db.User.update(
-    {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
-    .then((updateUser) => {
-      if (updateUser[0] === 0) {
-        res.status(404).json(updateUser);
-      } else {
-        console.log(updateUser);
-        res.json(updateUser);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-//DELETE a user
-app.delete("/api/users/:id", function (req, res) {
-  db.User.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((data) => {
-      if (data === 0) {
-        res.status(404).json(data);
-      } else {
-        res.json(data);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
 });
 
 // Syncing our sequelize models and then starting our Express app
